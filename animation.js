62b04b55bba3c90a3fa093ecdfe6a12e7411183f
Katrina <s941606@gmail.com>
2019-04-14 01:30:24 +0800

    fix artwork out

--- animation.js
+++ animation.js
@@ -1,16 +1,22 @@
 /*--------------------- Nav ------------------*/
+
 function closeMenu() {
   $('#wrapper').removeClass('toggled');
   $('#nav-icon2').removeClass('open');
+  $('.navbar').removeClass('open');
 }
 
 function openmenu() {
   $('#wrapper').addClass('toggled');
   $('#nav-icon2').addClass('open');
+  $('.navbar').removeClass('open');
 }
 
 //reload page when resize screen
-$(window).resize(function(){location.reload();});
+/* $(window).resize(function () {
+  location.reload();
+});
+*/
 
 $(document).ready(function () {
 
@@ -31,7 +37,6 @@ $(document).ready(function () {
     e.preventDefault();
     $('#wrapper').toggleClass('toggled');
     $('#nav-icon2').toggleClass('open');
-    //$('#menu-toggle').toggleClass('open');
     $('.navbar').toggleClass('open');
   });
 });
@@ -43,58 +48,38 @@ $(".list-group-item").hover(
     $(this).removeClass("active");
   }
 );
+
+
 /*--------------------- Artwork_out ------------------*/
-$(".artWork_item").hover(function(){
-  $('p', this).css("color","#bc2d2d");
-  $('.artwork_hr', this).css("border-color","#bc2d2d")
- 
- $('.artWorkPhoto', this).css("opacity", "0.2");
- $('.redBall', this).css("display", "block");
- $('.redBall', this).css("opacity", "1");
-  
-},function(){
-  $(".artWork_item p").css("color","#6f6155")
-  $(".artwork_hr").css("border-color","#6f6155")
+$(".artWork_item").hover(function () {
+  $('p', this).css("color", "#bc2d2d");
+  $('.artwork_hr', this).css("border-color", "#bc2d2d")
+
+  $('.artWorkPhoto', this).css("opacity", "0.2");
+  $('.redBall', this).css("display", "block");
+  $('.redBall', this).css("opacity", "1");
+
+}, function () {
+  $('p', this).css("color", "#6f6155");
+  $(".artWork_item p", this).css("color", "#6f6155")
+  $(".artwork_hr", this).css("border-color", "#6f6155")
   $('.artWorkPhoto', this).css("opacity", "1");
   $('.artWorkPhoto', this).css("opacity", "1");
   $('.redBall', this).css("display", "none");
- 
+
 })
 
 
 // change submenu red border
-$(".colLeft").click(function(){
-  $(".colLeft").css("border-color","#D8D8D8");
-  $(this).css("border-color","#bc2d2d");
+$(".colLeft").click(function () {
+  $(".colLeft").css("border-color", "#D8D8D8");
+  $(this).css("border-color", "#bc2d2d");
 
 })
-/*--------------------- Timer ------------------*/
-const fourthOfJuly = new Date("2019/06/02 14:30:00").getTime();
-// countdown
-let timer = setInterval(function () {
-
-  // get today's date
-  const today = new Date().getTime();
-
-  // get the difference
-  const diff = fourthOfJuly - today;
-
-  // math
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-  document.getElementById("days").textContent = days
-  document.getElementById("hours").textContent = hours
-  document.getElementById("minutes").textContent = minutes
-  document.getElementById("seconds").textContent = seconds
 
 
-}, 1000);
 
 
-/*--------------------- Show fixed vote panel------------------*/
 window.onload = function () {
 
   //close nav
@@ -112,8 +97,11 @@ window.onload = function () {
     console.log('mobile');
   }
 
+
+  /*--------------------- Show fixed vote panel------------------*/
   document.onmousewheel = function (e) {
-    //console.log(window.pageYOffset);
+    console.log(window.pageYOffset);
+
 
     if (window.pageYOffset > 200) {
       showFixedVote();
@@ -122,6 +110,7 @@ window.onload = function () {
     }
   }
 }
+
 
 function showFixedVote() {
   $(".shareButton").css("position", "fixed");
@@ -169,53 +158,3 @@ $(".latestNews a").hover(
     $(this).css("color", "#6f6155");
   }
 );
-
-/*-------------------------------------------------------------------------------
-	  featured slider
-	-------------------------------------------------------------------------------*/
-  if ($('.featured-carousel').length) {
-    $('.featured-carousel').owlCarousel({
-        loop: false,
-        margin: 30,
-        items: 1,
-        nav: true,
-        dots: false,
-        responsiveClass: true,
-        slideSpeed: 300,
-        paginationSpeed: 500,
-        navText : ["<div class='left-arrow'><i class='ti-angle-left'></i></div>","<div class='right-arrow'><i class='ti-angle-right'></i></div>"],
-        responsive: {
-            768: {
-                items: 2
-            },
-            1100: {
-                items: 3
-            }
-        }
-    })
-}
-
-
-
-/*-------------------------------------------------------------------------------
-featured slider
--------------------------------------------------------------------------------*/
-if ($('.hero-carousel').length) {
-    $('.hero-carousel').owlCarousel({
-        loop: false,
-        margin: 30,
-        items: 1,
-        nav: false,
-        dots: true,
-        responsiveClass: true,
-        slideSpeed: 300,
-        paginationSpeed: 500
-    })
-}
-
-
-
-
-});
-
-
---
+++ timer.js
@@ -0,0 +1,23 @@
+/*--------------------- Timer ------------------*/
+const fourthOfJuly = new Date("2019/06/02 14:30:00").getTime();
+// countdown
+let timer = setInterval(function () {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = fourthOfJuly - today;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+}, 1000);
